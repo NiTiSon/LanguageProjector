1729674b9d73b40f1fac5763e3486a14777d396e
NiTiSon <NiTiSon@users.noreply.github.com>
2026-06-03 03:50:16 +0300

    change footer to equal license

---
+++ LanguageProjector/Logic/Language.cs
@@ -0,0 +1,17 @@
+﻿namespace LanguageProjector.Logic;
+
+public sealed class Language
+{
+    public Guid Id { get; }
+
+    private Language(Guid id)
+    {
+        Id = id;
+    }
+
+    public static Language Create()
+    {
+        Guid id = Guid.CreateVersion7();
+        return new(id);
+    }
+}
